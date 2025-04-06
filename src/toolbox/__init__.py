f0f0bca5962b9b43061ccf9f9b1a248c8d606ac7
Germanicus75 <163282753+Germanicus75@users.noreply.github.com>
2025-04-06 09:17:18 +0200

    Update __init__.py

--- src/toolbox/__init__.py
+++ src/toolbox/__init__.py
@@ -1,1 +1,12 @@
 __version__ = "0.1.0"
+from flask import Flask
+import os
+
+def create_app():
+    app = Flask(__name__, template_folder=os.path.abspath('toolbox/templates'))
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    # Import et enregistrement des routes
+    app.register_blueprint(routes)
+
+return app
