67a561f8ae7632df3ce23a4018bb2a1cf6f5a1b9
Lynda HELLAL <lyndahellal@MacBook-Pro-de-Lynda.local>
2025-05-22 09:34:35 +0200

    Ajout du système d'authentification avec base de données SQLite et interfaces login/register

--- src/toolbox/scanner.py
+++
@@ -1,6 +0,0 @@
-from gvm.connections import TLSConnection
-from gvm.protocols.gmp import Gmp
-
-def get_vuln_scanner():
-    connection = TLSConnection(hostname='localhost', port=9390)
-    return Gmp(connection=connection)
